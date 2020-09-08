e1ea5c0b9c6c30b123b881ffbcb33e171f3e020c
roman kupry <64957283+kapkappa@users.noreply.github.com>
2020-09-09 02:56:29 +0300

    Add files via upload

--- Server.py
+++ Server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[31]:
+# In[74]:
 
 
 import flask
@@ -9,7 +9,7 @@ import time
 from datetime import datetime
 
 
-# In[59]:
+# In[75]:
 
 
 from flask import Flask
@@ -17,7 +17,10 @@ from flask import request
 
 app = Flask(__name__)
 db = []
+BLACK_LIST=[]
 unique_names = [] #Корректно заработает при наличии функции логина/пароля, т.е. созздания акаунта.
+PASSWORD_ADD = 'ADD_WORD_TO_BLACK_LIST'
+PASSWORD_REM = 'REMOVE_WORD_FROM_BLACK_LIST'
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -55,12 +58,25 @@ def send():
     data = request.json
     if data['name'] not in unique_names:
         unique_names.append(data['name'])
+    
+    if data['text'].find(PASSWORD_ADD) != -1:
+        data['text'] = data['text'].replace(PASSWORD_ADD+' ','')
+        BLACK_LIST.append(data['text'])
+    
+    if data['text'].find(PASSWORD_REM) != -1:
+        data['text'] = data['text'].replace(PASSWORD_REM+' ','')
+        BLACK_LIST.remove(data['text'])
+    
+    for word in BLACK_LIST:
+        data['text'] = data['text'].replace(word,'***')
+    
     db.append({
         'id' : len(db),
         'name' : data['name'],
         'text' : data['text'],
         'timestamp' : time.time()
     })
+        
     return {'OK':True}
 
 @app.route('/messages')
@@ -73,7 +89,7 @@ def messages():
     return {'messages': db[after_id : min(after_id + 50, len(db))]}
 
 
-# In[60]:
+# In[76]:
 
 
 app.run()
