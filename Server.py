df09d3f61954a6d9253d81f4326706917ffd7bd5
roman kupry <64957283+kapkappa@users.noreply.github.com>
2020-09-09 03:14:55 +0300

    Add files via upload

--- Server.py
+++ Server.py
@@ -9,7 +9,7 @@ import time
 from datetime import datetime
 
 
-# In[75]:
+# In[77]:
 
 
 from flask import Flask
@@ -19,8 +19,9 @@ app = Flask(__name__)
 db = []
 BLACK_LIST=[]
 unique_names = [] #Корректно заработает при наличии функции логина/пароля, т.е. созздания акаунта.
-PASSWORD_ADD = 'ADD_WORD_TO_BLACK_LIST'
-PASSWORD_REM = 'REMOVE_WORD_FROM_BLACK_LIST'
+PASSWORD_ADD = 'ADD_TO_BLACK_LIST'
+PASSWORD_REM = 'REM_FROM_BLACK_LIST'
+PASSWORD_SHW = 'SHOW_BIG_BLACK_LIST'
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -59,23 +60,31 @@ def send():
     if data['name'] not in unique_names:
         unique_names.append(data['name'])
     
-    if data['text'].find(PASSWORD_ADD) != -1:
-        data['text'] = data['text'].replace(PASSWORD_ADD+' ','')
-        BLACK_LIST.append(data['text'])
+    if data['text']==PASSWORD_SHW:
+        db.append({
+            'id' : len(db),
+            'name' : 'admin',
+            'text' : BLACK_LIST,
+            'timestamp' : time.time()
+        })
+    else:
+        if data['text'].find(PASSWORD_ADD) != -1:
+            data['text'] = data['text'].replace(PASSWORD_ADD+' ','')
+            BLACK_LIST.append(data['text'])
     
-    if data['text'].find(PASSWORD_REM) != -1:
-        data['text'] = data['text'].replace(PASSWORD_REM+' ','')
-        BLACK_LIST.remove(data['text'])
+        if data['text'].find(PASSWORD_REM) != -1:
+            data['text'] = data['text'].replace(PASSWORD_REM+' ','')
+            BLACK_LIST.remove(data['text'])
     
-    for word in BLACK_LIST:
-        data['text'] = data['text'].replace(word,'***')
+        for word in BLACK_LIST:
+            data['text'] = data['text'].replace(word,'***')
     
-    db.append({
-        'id' : len(db),
-        'name' : data['name'],
-        'text' : data['text'],
-        'timestamp' : time.time()
-    })
+        db.append({
+            'id' : len(db),
+            'name' : data['name'],
+            'text' : data['text'],
+            'timestamp' : time.time()
+        })
         
     return {'OK':True}
 
@@ -89,7 +98,7 @@ def messages():
     return {'messages': db[after_id : min(after_id + 50, len(db))]}
 
 
-# In[76]:
+# In[78]:
 
 
 app.run()
